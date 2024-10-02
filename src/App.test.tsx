14f8862659e3413bba38847083eee21f9c9a2740
Steven Orc <stevencloudorc@gmail.com>
2024-10-02 18:04:46 +0200

    start project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+test("renders App component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello Products/i);
   expect(linkElement).toBeInTheDocument();
 });
